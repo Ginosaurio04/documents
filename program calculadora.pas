f736c1444a54c36505fe541a53b3477361b229ed
Gino Cova <gcova.1249@unimar.edu.ve>
2024-10-16 19:53:26 -0400

    mi primer commit

---
+++ program calculadora.pas
@@ -0,0 +1,11 @@
+program calculadora;
+var
+n1,n2, resultado: integer
+begin
+  writeln('ingrese el primer numero');
+  readln(n1);
+  writeln('ingrese el segundo numero');
+  readln('n2');
+  resultado
+  writeln('n1+n2', resultado:=)
+end.
